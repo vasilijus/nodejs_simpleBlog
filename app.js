225a56de686b3990e1e6bd7aa8986ced1948b08b
vasilijus <vasiliokas@gmail.com>
2018-09-19 21:17:27 +0100

    adding post.js to the app config., or to be more exact to the express event

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
+const postRouter = require('./routes/post') //location
 const keys = require('./keys')
 
 const port = process.env.PORT || 5000
@@ -11,6 +12,7 @@ mongoose.connect(keys.mongoURI)
     .catch( err => console.error(err) )
 
 const app = express()
+app.use('/api/post', postRouter) //URL prefix , router which we want to use
 app.use(express.static(clientPath))
 app.listen(port, () => {
     console.log(`Server has been started on port ${port}`)
