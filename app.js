8944d905406355189689201854779f494cd71085
vasilijus <vasiliokas@gmail.com>
2018-09-23 18:05:43 +0100

    finished video 3 , made GET, POST, DELETE

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
-const postRouter = require('./routes/post') //location
+const postRouter = require('./routes/post')
 const keys = require('./keys')
 
 const port = process.env.PORT || 5000
-const clientPath = path.join(__dirname, 'client')
-
+const clientPath = path.join(__dirname, 'client') // set path to index.html  => dir -> client -> index
+// address bazy danih
 mongoose.connect(keys.mongoURI)
-    .then( () => console.log('MongoDb connected.') )
-    .catch( err => console.error(err) )
+    .then( () => console.log('mongoDB - conneced.') )
+    .catch(err => console.error(err) )
+
 
 const app = express()
-app.use('/api/post', postRouter) //URL prefix , router which we want to use
+app.use('/api/post', postRouter)
 app.use(express.static(clientPath))
-app.listen(port, () => {
-    console.log(`Server has been started on port ${port}`)
+
+app.listen( port , () => {
+    console.log(`Server has been started on: ${port}`)
 })
